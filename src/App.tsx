624fd0a5bddff3ed07726585d805c2a7a86b1885
Daniel Araujo Chaves <danielaraujo.da789@gmail.com>
2026-09-01 11:25:42 -0300

    ✨ New Feature: Configura rotas

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,29 @@ import {
 
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
-
 import Home from "./pages/home/Home";
 
-import ListaCategorias from "./components/categorias//listacategoria/ListaCategoria";
+import ListaCategorias from "./components/categorias/listacategoria/ListaCategoria";
 import FormCategoria from "./components/categorias/formcategoria/FormCategoria";
 import DeletarCategoria from "./components/categorias/deletarcategoria/DeletarCategoria";
 
 function App() {
-
   return (
     <BrowserRouter>
-      <div className="
-        min-h-screen
-        flex
-        flex-col
-        bg-gray-50
-      ">
+      <div className="min-h-screen flex flex-col bg-gray-50">
         <Navbar />
+
         <main className="flex-1">
           <Routes>
-            <Route path="/"element={<Home />} />
-            <Route path="/home"element={<Home />} />
-            <Route path="/categorias"element={<ListaCategorias />} />
-            <Route path="/cadastrarcategoria"element={<FormCategoria />} />
-            <Route path="/editarcategoria/:id"element={<FormCategoria />} />
-            <Route path="/deletarcategoria/:id"element={<DeletarCategoria />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/categorias" element={<ListaCategorias />} />
+            <Route path="/cadastrarcategoria" element={<FormCategoria />} />
+            <Route path="/editarcategoria/:id" element={<FormCategoria />} />
+            <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
           </Routes>
         </main>
+
         <Footer />
       </div>
     </BrowserRouter>
